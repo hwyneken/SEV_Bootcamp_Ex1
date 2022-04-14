d63bc71317384d51093fb50fbaf852b5c9b70a1f
hwyneken <hwyneken@gmail.com>
2022-04-14 15:17:53 -0500

    final replication basic changes Apr 14 2022

---
+++ replication_basic/Create_Theme_Object.R
@@ -0,0 +1,13 @@
+require(ggplot2)
+require(ggthemes)
+
+sevBootcamp1Theme <- theme_tufte()
+sevBootcamp1Theme <- sevBootcamp1Theme + theme(axis.text = element_text(size=12),
+                           axis.title = element_text(size=14),
+                           title = element_text(size=16),
+                           legend.title = element_text(size=14),
+                           legend.text = element_text(size=12),
+                           strip.text = element_text(size=14),
+                           panel.background = element_rect(fill="#f8f8f8"),
+                           strip.background = element_rect(fill="gray"))
+saveRDS(sevBootcamp1Theme,file = "data/sevBootcamp1Theme.RDS")
